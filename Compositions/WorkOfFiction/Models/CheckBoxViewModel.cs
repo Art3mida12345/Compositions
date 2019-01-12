3475d6b0609fbaa4f4c9a1c284afea7da51c10c0
Oleksii Korniienko <Oleksii_Korniienko@epam.com>
2019-01-12 22:26:26 +0200

    Create Filter view

---
+++ Compositions/WorkOfFiction/Models/CheckBoxViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkOfFiction.Models
+{
+    public class CheckBoxViewModel
+    {
+        public int Id { get; set; }
+
+        public bool IsChecked { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ Compositions/WorkOfFiction/Models/CompositionFilter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+
+namespace WorkOfFiction.Models
+{
+    public class CompositionFilter
+    {
+        public List<CheckBoxViewModel> Genres { get; set; }
+
+        public List<CheckBoxViewModel> Authors { get; set; }
+
+        public List<CheckBoxViewModel> Types { get; set; }
+
+        public List<CheckBoxViewModel> Langs { get; set; }
+
+
+        public List<int> SelectedGenres { get; set; }
+
+        public List<int> SelectedAuthors { get; set; }
+
+        public List<int> SelectedTypes { get; set; }
+
+        public List<int> SelectedLangs { get; set; }
+
+        public  CompositionFilter()
+        {
+            Genres = new List<CheckBoxViewModel>();
+            Authors = new List<CheckBoxViewModel>();
+            Types = new List<CheckBoxViewModel>();
+            Langs = new List<CheckBoxViewModel>();
+
+            SelectedGenres= new List<int>();
+            SelectedAuthors= new List<int>();
+            SelectedTypes= new List<int>();
+            SelectedLangs= new List<int>();
+        }
+
+    }
+}
