6b8428ac3e0d6b1e519201a5a1c3f066132b3a08
Maryna Kudriavtseva <art3mida12345@gmail.com>
2019-01-12 14:57:23 +0200

    add country service

--- Compositions/WorkOfFiction/Controllers/CountryController.cs
+++ Compositions/WorkOfFiction/Controllers/CountryController.cs
@@ -1,9 +1,17 @@
 ﻿using System.Web.Mvc;
+using WorkOfFiction.Helpers;
 
 namespace WorkOfFiction.Controllers
 {
     public class CountryController : Controller
     {
+        private readonly Count _oracleHelper;
+
+        public CountryController()
+        {
+            _oracleHelper = new OracleHelper();
+        }
+
         // GET: Country
         public ActionResult Index()
         {
---
+++ Compositions/WorkOfFiction/Services/CountryService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using WorkOfFiction.Helpers;
+using WorkOfFiction.Models;
+
+namespace WorkOfFiction.Services
+{
+    public class CountryService
+    {
+        private readonly OracleHelper _oracleHelper;
+
+        public CountryService()
+        {
+            _oracleHelper = new OracleHelper();
+        }
+
+        public IEnumerable<Country> GetAll()
+        {
+            var countries = _oracleHelper.GetAllCountries();
+
+            return countries;
+        }
+
+        public Country GetCountry(int? id)
+        {
+            var country = _oracleHelper.GetCountry(id);
+
+            return country;
+        }
+
+        public void Insert(Country country)
+        {
+
+        }
+    }
+}
