761e7325568f99a0497586928129286714b39384
Maryna Kudriavtseva <art3mida12345@gmail.com>
2019-01-13 16:33:34 +0200

    add validation attribute

--- Compositions/WorkOfFiction/ViewModels/AuthorViewModel.cs
+++ Compositions/WorkOfFiction/ViewModels/AuthorViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace WorkOfFiction.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkOfFiction.ViewModels
 {
     public class AuthorViewModel
     {
         public int? Id { get; set; }
+        [Display(Name="Full Name")]
         public string FullName { get; set; }
     }
 }
